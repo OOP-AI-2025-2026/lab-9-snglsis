c332648ab6a9f064b7b33fdaa35964814ea43253
snglsis <linaprokhorova07@gmail.com>
2025-11-15 23:15:18 +0200

    lab 9 is done

--- app/src/main/java/ua/opnu/Task.java
+++ app/src/main/java/ua/opnu/Task.java
@@ -1,14 +1,21 @@
 package ua.opnu;
 
-
-import java.util.*;
-
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Task {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
     }
 
+    // Завдання 1
     public void removeShorterStrings(List<String> list) {
         ArrayList<Integer> toRemove = new ArrayList<>();
 
@@ -27,6 +34,7 @@ public class Task {
         }
     }
 
+    // Завдання 2
     public void stutter(List<String> list) {
         for (int i = 0; i < list.size(); i += 2) {
             String value = list.get(i);
@@ -34,17 +42,22 @@ public class Task {
         }
     }
 
+    // Завдання 3
     public void switchPairs(List<String> list) {
         for (int i = 0; i < list.size() - 1; i += 2) {
-            String t = list.get(i);
+            String tmp = list.get(i);
             list.set(i, list.get(i + 1));
-            list.set(i + 1, t);
+            list.set(i + 1, tmp);
         }
     }
 
+    // Завдання 4
     public void removeDuplicates(List<String> list) {
-        for (int i = 0; i < list.size() - 1; ) {
-            if (list.get(i).equals(list.get(i + 1))) {
+        int i = 0;
+        while (i < list.size() - 1) {
+            String cur = list.get(i);
+            String next = list.get(i + 1);
+            if (cur.equals(next)) {
                 list.remove(i + 1);
             } else {
                 i++;
@@ -52,36 +65,49 @@ public class Task {
         }
     }
 
+    // Завдання 5
     public void markLength4(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() == 4) {
+            String value = list.get(i);
+            if (value.length() == 4) {
                 list.add(i, "****");
                 i++;
             }
         }
     }
 
+    // Завдання 6
     public boolean isPalindrome(Queue<Integer> queue) {
-        java.util.ArrayDeque<Integer> stack = new java.util.ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
         int size = queue.size();
 
-        for (int i = 0; i < size; i++) {
-            int x = queue.remove();
-            queue.add(x);
-            stack.push(x);
+        if (size <= 1) {
+            return true;
         }
 
-        boolean ok = true;
         for (int i = 0; i < size; i++) {
-            int a = queue.remove();
-            int b = stack.pop();
-            if (a != b) ok = false;
-            queue.add(a);
+            Integer value = queue.remove();
+            stack.push(value);
+            queue.add(value);
         }
 
-        return ok;
+        boolean result = true;
+
+        for (int i = 0; i < size; i++) {
+            Integer fromQueue = queue.remove();
+            Integer fromStack = stack.pop();
+
+            if (!fromQueue.equals(fromStack)) {
+                result = false;
+            }
+
+            queue.add(fromQueue);
+        }
+
+        return result;
     }
 
+    // Завдання 7
     public void reorder(Queue<Integer> queue) {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int size = queue.size();
@@ -110,99 +136,185 @@ public class Task {
         }
     }
 
-
+    // Завдання 8
     public void rearrange(Queue<Integer> queue) {
-        Queue<Integer> odd = new LinkedList<>();
-        Queue<Integer> even = new LinkedList<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int size = queue.size();
 
-        while (!queue.isEmpty()) {
-            int x = queue.remove();
-            if (x % 2 != 0) odd.add(x);
-            else even.add(x);
+        if (size <= 1) {
+            return;
         }
 
-        queue.addAll(odd);
-        queue.addAll(even);
+        for (int i = 0; i < size; i++) {
+            Integer x = queue.remove();
+            if (x % 2 != 0) {
+                stack.push(x);
+            } else {
+                queue.add(x);
+            }
+        }
+
+        int evenCount = queue.size();
+        int oddCount = stack.size();
+
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+
+        for (int i = 0; i < evenCount; i++) {
+            queue.add(queue.remove());
+        }
+
+        for (int i = 0; i < oddCount; i++) {
+            stack.push(queue.remove());
+        }
+
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
     }
 
-
+    // Завдання 9
     public int maxLength(Set<String> set) {
+        if (set.isEmpty()) {
+            return 0;
+        }
+
         int max = 0;
         for (String s : set) {
-            if (s.length() > max) max = s.length();
+            int len = s.length();
+            if (len > max) {
+                max = len;
+            }
         }
         return max;
     }
 
+    // Завдання 10
     public void removeEvenLength(Set<String> set) {
-        set.removeIf(s -> s.length() % 2 == 0);
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            if (s.length() % 2 == 0) {
+                it.remove();
+            }
+        }
     }
 
+    // Завдання 11
     public int numInCommon(List<Integer> list1, List<Integer> list2) {
-        java.util.Set<Integer> a = new java.util.HashSet<>(list1);
-        java.util.Set<Integer> res = new java.util.HashSet<>();
+        Set<Integer> first = new HashSet<>(list1);
+        Set<Integer> second = new HashSet<>(list2);
+        int count = 0;
 
-        for (int x : list2) {
-            if (a.contains(x)) res.add(x);
+        for (Integer value : first) {
+            if (second.contains(value)) {
+                count++;
+            }
         }
-        return res.size();
+
+        return count;
     }
 
+    // Завдання 12
     public boolean isUnique(Map<String, String> map) {
-        return new java.util.HashSet<>(map.values()).size() == map.size();
+        Set<String> values = new HashSet<>();
+        for (String v : map.values()) {
+            if (values.contains(v)) {
+                return false;
+            }
+            values.add(v);
+        }
+        return true;
     }
 
-    public Map<String, Integer> intersect(Map<String, Integer> map1, Map<String, Integer> map2) {
-        Map<String, Integer> out = new java.util.HashMap<>();
-        for (var e : map1.entrySet()) {
-            if (map2.containsKey(e.getKey()) && map2.get(e.getKey()).equals(e.getValue())) {
-                out.put(e.getKey(), e.getValue());
+    // Завдання 13
+    public Map<String, Integer> intersect(Map<String, Integer> map1,
+                                          Map<String, Integer> map2) {
+        Map<String, Integer> result = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : map1.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            if (map2.containsKey(key) && value.equals(map2.get(key))) {
+                result.put(key, value);
             }
         }
-        return out;
+
+        return result;
     }
 
+    // Завдання 14
     public Map<String, Integer> reverse(Map<Integer, String> map) {
-        Map<String, Integer> out = new java.util.HashMap<>();
-        for (var e : map.entrySet()) {
-            out.put(e.getValue(), e.getKey());
+        Map<String, Integer> result = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            result.put(value, key);
         }
-        return out;
+
+        return result;
     }
 
+    // Завдання 15
     public int rarest(Map<String, Integer> map) {
-        if (map.isEmpty()) return 0;
-
-        Map<Integer, Integer> freq = new java.util.HashMap<>();
-        for (int val : map.values()) {
-            freq.put(val, freq.getOrDefault(val, 0) + 1);
+        if (map.isEmpty()) {
+            return 0;
         }
 
-        int bestVal = Integer.MAX_VALUE;
-        int bestCnt = Integer.MAX_VALUE;
+        Map<Integer, Integer> freq = new HashMap<>();
 
-        for (var e : freq.entrySet()) {
-            int v = e.getKey();
-            int c = e.getValue();
-            if (c < bestCnt || (c == bestCnt && v < bestVal)) {
-                bestCnt = c;
-                bestVal = v;
+        for (Integer value : map.values()) {
+            Integer old = freq.get(value);
+            if (old == null) {
+                freq.put(value, 1);
+            } else {
+                freq.put(value, old + 1);
             }
         }
-        return bestVal;
+
+        int bestValue = Integer.MAX_VALUE;
+        int bestCount = Integer.MAX_VALUE;
+
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int value = entry.getKey();
+            int cnt = entry.getValue();
+
+            if (cnt < bestCount || (cnt == bestCount && value < bestValue)) {
+                bestCount = cnt;
+                bestValue = value;
+            }
+        }
+
+        return bestValue;
     }
 
+    // Завдання 16
     public int maxOccurrences(List<Integer> list) {
-        if (list.isEmpty()) return 0;
+        if (list.isEmpty()) {
+            return 0;
+        }
 
-        Map<Integer, Integer> freq = new java.util.HashMap<>();
-        for (int x : list) freq.put(x, freq.getOrDefault(x, 0) + 1);
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (Integer value : list) {
+            Integer old = freq.get(value);
+            if (old == null) {
+                freq.put(value, 1);
+            } else {
+                freq.put(value, old + 1);
+            }
+        }
 
         int max = 0;
-        for (int c : freq.values()) max = Math.max(max, c);
+        for (Integer cnt : freq.values()) {
+            if (cnt > max) {
+                max = cnt;
+            }
+        }
 
         return max;
     }
 }
-
-
